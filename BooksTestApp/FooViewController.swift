5a700eaf838e5aed2cfc53f72370bdf64a1a528b
Evgeniia Kiriushina <evgeniya.kiriushina@axonista.com>
2022-07-07 16:43:27 +0200

    refactoring is done

--- BooksTestApp/FooViewController.swift
+++ BooksTestApp/FooViewController.swift
@@ -5,47 +5,47 @@
 //  Created by Evgeniia Kiriushina on 07.07.2022.
 //
 
-struct Book {
+import UIKit
+
+protocol Book { // created protocol instead cause each ComicBook is Book, but not each Book is ComicBook
+    var isbn: String? { get set }
+    var title: String? { get set }
+    var author: String? { get set }
+}
+
+struct ComicBook: Book {
     var isbn: String?
     var title: String?
     var author: String?
-}
-struct ComicBook {
-    var isbn: String?
-    var author: String?
+
     var genre: String?
 }
-class Foo {
-    init() { }
-    func fetchBooks() -> [Book] {
-        // Fetch from DB
-    }
-        
-    func saveBooks(books: [Books]) {
-        // Save on DB
-    }
-}
+
 class NetworkManager {
     static let shared = NetworkManager()
     var isFetching: Bool = false
     private init() {
         // Setup ...
     }
-    func getBooks() -> [Book] {
-        // ...
+    func fetchBooks() -> [Book] { // removed getBooks() cause method is the same like fetchBooks()
+        // Fetch from DB
     }
-    func getComicBooks() -> [ComicBook] {
+    func saveBooks(books: [Book]) {
+        // Save on DB
+    }
+    func getComicBooks() -> [ComicBook] { // moved this method here cause all actions with books are in this class
         // ...
     }
 }
-class FooViewController {
-    var foo = Foo()
+
+class FooViewController: UIViewController {
     var networkManager = NetworkManager.shared
     var data: [Book] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        data = foo.fetchBooks()
-        foo.saveBooks(books: data)
+        data = networkManager.fetchBooks() // we don't need the Foo class anymore
+        networkManager.saveBooks(books: data)
         // Reload view controller, table view / collection, etc...
         // ...
     }
