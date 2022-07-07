2c05746d9e67fd4859e76587ed9677e437f5f178
Evgeniia Kiriushina <evgeniya.kiriushina@axonista.com>
2022-07-07 13:46:23 +0200

    Initial Commit

---
+++ BooksTestApp/BooksTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksTestAppApp.swift
+//  BooksTestApp
+//
+//  Created by Евгения Колдышева on 07.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BooksTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
